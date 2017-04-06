c421af9765b495941080cc48bd7b732700d69f1a
daniel54ad <daniel54ad@gmail.com>
2017-04-06 16:30:27 +0800

    move get_schema

---
+++ parser/line.py
@@ -0,0 +1,38 @@
+import os
+
+parserList = os.listdir('.')
+parserList.remove('get_paper.py')
+parserList.remove('line.py')
+parserList.remove('main.py')
+#forMAC
+parserList.remove('.DS_Store')
+
+for f in parserList:
+    courtList = os.listdir(f)
+    #forMAC
+    courtList.remove('.DS_Store')
+    for g in courtList:
+        path = f+'/'+g
+
+        files = os.listdir(path)
+        #forMAC
+        files.remove('.DS_Store')
+
+
+        for f in files:
+            print (f)
+            fread = open(path+'/'+f ,'r')
+            for i in range(4):
+                content = fread.readline()
+                if content.find('法院') > 0:
+                    content = content[:content.find('法院')+2]
+                    print (content)
+                if content[0:6] == '【裁判字號】':
+                    content = content.replace('【裁判字號】','').replace('\n','')
+                    print (content)
+                if content[0:6] == '【裁判日期】':
+                    content = content.replace('【裁判日期】','').replace('\n','')
+                    print (content)
+                if content[0:6] == '【裁判案由】':
+                    content = content.replace('【裁判案由】','').replace('\n','')
+                    print (content)
---
+++ parser/main.py
@@ -0,0 +1,47 @@
+import os
+
+parserList = os.listdir('.')
+parserList.remove('get_paper.py')
+parserList.remove('line.py')
+parserList.remove('main.py')
+#forMAC
+parserList.remove('.DS_Store')
+
+for f in parserList:
+    courtList = os.listdir(f)
+    #forMAC
+    courtList.remove('.DS_Store')
+    for g in courtList:
+        path = f+'/'+g
+
+        files = os.listdir(path)
+        #forMAC
+        files.remove('.DS_Store')
+
+        for f in files:
+            fread = open(path+'/'+f ,'r')
+            for i in range(5):
+                lines = fread.readline()
+            content = fread.read()
+            main = content.find('主文')
+            content = content[main:]
+            fact = content.find('事實')
+            reason = content.find('理由')
+            mainContent = ''
+            if (reason-fact) == 3:
+                if content[fact-2:fact] == '犯罪':
+                    mainContent = content[2:fact-2]
+                else:
+                    mainContent = content[2:fact]
+            elif reason > fact:
+                if fact != -1:
+                    mainContent = content[2:fact]
+                else:
+                    mainContent = content[2:reason]
+            elif fact > reason:
+                if reason != -1:
+                    mainContent = content[2:reason]
+                else:
+                    mainContent = content[2:fact]
+            print (f)
+            print (mainContent)
