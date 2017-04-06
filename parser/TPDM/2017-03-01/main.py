2e231f33e7ee532281f5467dd3ba2b6b0f664325
daniel54ad <daniel54ad@gmail.com>
2017-04-06 10:53:21 +0800

    line and main

--- parser/TPDM/2017-03-01/main.py
+++ parser/TPDM/2017-03-01/main.py
@@ -18,23 +18,24 @@ for f in files:
         lines = fread.readline()
     content = fread.read()
     main = content.find('主文')
+    content = content[main:]
     fact = content.find('事實')
     reason = content.find('理由')
     mainContent = ''
     if (reason-fact) == 3:
         if content[fact-2:fact] == '犯罪':
-            mainContent = content[main+2:fact-2]
+            mainContent = content[2:fact-2]
         else:
-            mainContent = content[main+2:fact]
+            mainContent = content[2:fact]
     elif reason > fact:
         if fact != -1:
-            mainContent = content[main+2:fact]
+            mainContent = content[2:fact]
         else:
-            mainContent = content[main+2:reason]
+            mainContent = content[2:reason]
     elif fact > reason:
         if reason != -1:
-            mainContent = content[main+2:reason]
+            mainContent = content[2:reason]
         else:
-            mainContent = content[main+2:fact]
+            mainContent = content[2:fact]
     print (f)
     print (mainContent)
