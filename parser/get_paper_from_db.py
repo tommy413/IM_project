9a576f2c3d7dad23b1f68450897e40bf902d03ad
Tommy413 <tommy199685413@gmail.com>
2017-05-18 12:25:49 +0800

    with file argument

--- parser/get_paper_from_db.py
+++ parser/get_paper_from_db.py
@@ -1,6 +1,7 @@
 # -*- coding: utf8 -*-
 from BeautifulSoup import BeautifulSoup
 import sys
+import csv
 import time
 import os
 import datetime
@@ -9,6 +10,7 @@ import psycopg2
 import logging
 
 conn = psycopg2.connect(database="law1", user="datac1", password="datac15543", host="ci.lu.im.ntu.edu.tw", port="5432")
+conn.set_session(autocommit=True)
 
 def parse(paperid,html):
 	info = ""
@@ -36,59 +38,82 @@ def parse(paperid,html):
 							paper = paper + str(td.pre.string).replace('	','').replace(' ','').replace(' ','').replace('\t','').replace('　','')
 							flag = 1
 	except Exception as e:
-		logging.error("%s_parse_error_%s\r\n" % (paperid,e) )
+		logging.error("%s_parse_error : %s\r\n" % (paperid,e) )
 		return "error"
-	return paper
+	else :
+		if "裁判全文" not in paper:
+			logging.error("%s_data_error : without paper\r\n" % (paperid) )
+			return "error"
+		else :
+			return paper
 
 def DB_Output(conn,paperid,paper):
-	
+	paper = paper.replace("'","''")
 	db = conn.cursor()
-	db.execute("SELECT COUNT(id) from raw_html where id = \'%s\'" % (paperid))
+	db.execute("SELECT COUNT(id) from htmldata where id = \'%s\'" % (paperid))
 	count = int(db.fetchall()[0][0])
-	if count == 0 :
-		query = "INSERT INTO HTMLDATA (ID,CONTENT) VALUES (\'%s\' , \'%s\')" % (paperid,paper)
+	if count == 1 :
+		query = "UPDATE htmldata SET content = \'%s\' where id = \'%s\'" % (paper,paperid)
 	else :
-		query = "UPDATE HTMLDATA SET CONTENT = \'%s\' " % (paper)
+		query = "INSERT INTO htmldata (id,content) VALUES (\'%s\' , \'%s\')" % (paperid,paper)
 		
 	try:
 		db.execute(query)
 		conn.commit()
 	except Exception as e:
-		logging.error("%s_commit_error_%s\r\n" % (paperid,e))
+		logging.error("%s_commit_error : %s\r\n" % (paperid,e))
 		return True
 	else:
-		if count == 0:
-			logging.info("%s inserted\r\n" % paperid)
-		else :
+		if count == 1:
 			logging.info("%s updated\r\n" % paperid)
+		else :
+			logging.info("%s inserted\r\n" % paperid)
 		return False
 
-papertype = sys.argv[1]
-begindate = datetime.datetime(int(sys.argv[2][:4]),int(sys.argv[2][4:6]),int(sys.argv[2][6:]))
-enddate = datetime.datetime(int(sys.argv[3][:4]),int(sys.argv[3][4:6]),int(sys.argv[3][6:]))
-if not os.path.exists("log"):
-	os.makedirs("log")
-logging.basicConfig(filename="log/%s_%s_%s.log" % (sys.argv[1],sys.argv[2],sys.argv[3]), level=logging.INFO)
+papertype_list = []
+if sys.argv[1]=="-c":
+	papertype_list = [sys.argv[2]]
+if sys.argv[1]=="-f":
+	papertype_list = list(csv.reader(open(sys.argv[2],'r')))[0]
+
+begindate = datetime.datetime(int(sys.argv[3][:4]),int(sys.argv[3][4:6]),int(sys.argv[3][6:]))
+enddate = datetime.datetime(int(sys.argv[4][:4]),int(sys.argv[4][4:6]),int(sys.argv[4][6:]))
 db = conn.cursor()
 
-for i in range(int((enddate-begindate).days)+1):
-	single_day=begindate+timedelta(i)
- 	queryid = papertype+str(single_day.date().strftime('%Y%m%d'))
- 	
- 	query = "SELECT id,casehtml from raw_html where id Like \'"+queryid+"%\'"
- 	# query = "SELECT id,casehtml from raw_html where id = 'TPDM20170301044' "
-	db.execute(query)
-	rows = db.fetchall()
-	error_list = []
+# logging.basicConfig(level = logging.ERROR ,format='%(asctime)s: %(levelname)s - %(message)s')
+formatter = logging.Formatter('%(asctime)s: %(levelname)s - %(message)s')
+logging.getLogger('').setLevel(logging.INFO)
 
-	for html in rows:
-		doc = html[1]
-		paperid = html[0]
-		paper = parse(paperid,doc)
-		if paper == "error":
-			continue
-		if DB_Output(conn,paperid,paper) :
-			error_list.append(paperid)
-	logging.info(str(queryid) + " Complete.\r\n")
-	logging.info("Error_List : \r\n%s\r\n" % str(error_list))
+for papertype in papertype_list:
+	for i in range(int((enddate-begindate).days)+1):
+		single_day=begindate+timedelta(i)
+		if not os.path.exists("log/%s" % papertype):
+			os.makedirs("log/%s" % papertype)
+		logfile_filehandler = logging.FileHandler("log/%s/%s.log" % (papertype,str(single_day.date().strftime('%Y%m%d'))))
+		logfile_filehandler.setLevel(logging.INFO)
+		logfile_filehandler.setFormatter(formatter)
+		logging.getLogger('').addHandler(logfile_filehandler)
+
+	 	queryid = papertype+str(single_day.date().strftime('%Y%m%d'))
+	 	
+	 	query = "SELECT id,casehtml from raw_html where id Like \'"+queryid+"%\'"
+		db.execute(query)
+		rows = db.fetchall()
+		error_list = []
+
+		for html in rows:
+			doc = html[1]
+			paperid = html[0]
+			paper = parse(paperid,doc)
+			if paper == "error":
+				error_list.append(paperid)
+				continue
+			flag = DB_Output(conn,paperid,paper)
+			if flag==True :
+				error_list.append(paperid)
+		logging.info(str(queryid) + " Complete.\r\n")
+		logging.error("Error_List : \r\n%s\r\n" % str(error_list))
+		logging.getLogger('').removeHandler(logfile_filehandler)
+		logfile_filehandler.close()
+
 conn.close()
